c41fef151e607ba9148d3327e711c23c4cec4e73
Vivek Shukla <shuklv4@medtronic.com>
2024-10-17 09:29:29 +0530

    chore(frmr): longest common prefix
    longest common prefix solution using JavaScript

--- string/valid-parentheses.js
+++ string/valid-parentheses.js
@@ -2,44 +2,42 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {
+let isValid = function (s) {
   if (s) {
     const arrStack = [];
     const openingBrackets = ["(", "{", "["];
     let elemToCheck = "";
-    for (char of s) {
+    for (let char of s) {
       elemToCheck = "";
       if (openingBrackets.includes(char)) {
         arrStack.push(char);
-      } else {
-        if (arrStack.length > 0) {
-          switch (char) {
-            case "}": {
-              elemToCheck = "{";
-              break;
-            }
-            case ")": {
-              elemToCheck = "(";
-              break;
-            }
-            case "]": {
-              elemToCheck = "[";
-              break;
-            }
+      } else if (arrStack.length > 0) {
+        switch (char) {
+          case "}": {
+            elemToCheck = "{";
+            break;
           }
+          case ")": {
+            elemToCheck = "(";
+            break;
+          }
+          case "]": {
+            elemToCheck = "[";
+            break;
+          }
+        }
 
-          if (arrStack[arrStack.length - 1] === elemToCheck) {
-            arrStack.pop();
-          } else {
-            return false;
-          }
+        if (arrStack[arrStack.length - 1] === elemToCheck) {
+          arrStack.pop();
         } else {
           return false;
         }
+      } else {
+        return false;
       }
     }
 
-    return arrStack.length === 0 ? true : false;
+    return arrStack.length === 0;
   }
 
   return false;
